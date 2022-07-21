d99beb802e9e1066f110c8105bd7e1d13e4ba4da
narafu <narafu@kakao.com>
2022-07-21 21:02:27 +0900

    섹션 5까지

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,13 +1,25 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuerydslApplication.class, args);
+    }
+
+    /**
+     * 싱글톤이지만 멀티스레드 환경에서도 동시성 걱정 안해도 된다.
+     */
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 
 }
